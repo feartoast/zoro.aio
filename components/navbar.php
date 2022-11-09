5dc6d2549fa3e288d1598bfdeaf1f6ec880c86c6
feartoast <63493692+feartoast@users.noreply.github.com>
2022-11-09 12:00:50 +0530

    NAVBAR
    site

---
+++ components/navbar.php
@@ -0,0 +1,19 @@
+<?php
+$a = new FORM();
+?>
+<div class="navbar box_wrap">
+    <div class="row">
+        <div class="col-6">
+            <div class="row">
+                <div class="col"><a href="">Home</a></div>
+                <div class="col"><a href="">Movies</a></div>
+                <div class="col"><a href="">TV Series</a></div>
+                <div class="col"><a href="">Most Popular</a></div>
+                <div class="col"><a href="">Top Airing</a></div>
+            </div>
+
+        </div>
+        <div class="col-6"></div>
+
+    </div>
+</div>
--- components/pre.php
+++ components/pre.php
@@ -1,4 +1,4 @@
 <?php
 $c = new CONTENT();
-$c->pre_html( '', '', 'bootstrap/css/bootstrap-grid', 'icons,table,tabs,buttons,alerts,inputs,color', 'zoro', 'jquery', '' );
+$c->pre_html( '', '', 'bootstrap/css/bootstrap-grid', 'icons,table,tabs,buttons,alerts,inputs,color', 'zoro','jquery', '' );
 ?>
